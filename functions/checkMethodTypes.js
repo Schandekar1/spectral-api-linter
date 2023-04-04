3ab848cb7a77a0bd75170c3537e770929831b699
Sachin Chandekar <124021644+Schandekar1@users.noreply.github.com>
2023-04-04 15:35:55 +0530

    Update checkMethodTypes.js

--- functions/checkMethodTypes.js
+++ functions/checkMethodTypes.js
@@ -1,7 +1,30 @@
 module.exports = input => {
-      return [
-        {
-          message: 'This is test error message from JavaScript function',
-        },
-      ];
-  };
+  if (input.servers == null || input.servers.length == 0) {
+    return [
+      {
+        message: 'OpenAPI "servers" must be present and non-empty array',
+      },
+    ];
+  } else if(input.paths ==null || input.paths.length == 0){
+    return [
+      {
+        message: 'OpenAPI "paths" must be present and non-empty object',
+      }
+    ];
+  }
+   else {
+    let errors=[];
+    const urlRegEx=/(https?):\/\/([a-z0-9-]+\.)+[a-z]+(:[0-9]+)?\/[a-zA-Z0-9_-]+\/V[0-9]+\/[a-zA-Z0-9_-]+.*$/i
+    input.servers.forEach(element => {
+      for (const [key, value] of Object.entries(input.paths)) {
+        console.log(`${element.url}${key}`);
+        if(urlRegEx.test(`${element.url}${key}`)===false){
+          errors= [...errors, {
+            message: `API Url structure ${element.url}${key} does not follow the standard format`,
+          }]
+        }
+      }
+    });
+    return errors;
+  }
+};
