4912f6e4e98ef616f73141ca600db28ab842f395
Sachin Chandekar <124021644+Schandekar1@users.noreply.github.com>
2023-04-18 14:58:24 +0530

    Create eorPublish.sh

---
+++ eorPublish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+COMMIT=$(git log --name-only --pretty=oneline --full-index HEAD^^..HEAD | grep -vE '^[0-9a-f]{40} ' | sort | uniq)
+
+  FILES=($COMMIT)
+  len=${#FILES[@]}
+
+  for (( i=0; i<${len}; i++)); do
+    DIR=$(dirname "${FILES[$i]}")
+    echo "Processing the file ${FILES[$i]} from the directory ${DIR}"
+    
+    if [[ ${DIR} == 'services/'* ]]; then
+      EXTENSION=$(echo ${FILES[$i]#*.})
+      if [ ${EXTENSION} == "yaml" ]; then
+        FILE=$(basename ${FILES[$i]} | cut -f 1 -d '.')
+        echo "commited file:=> ${DIR}/${FILE}.yaml"
+        
+        # ${SCRIPT_PATH}/generateFile.sh ${DIR}/${FILE}.yaml ${OpenAPI_TEMPLATE} "generated/${DIR}/${FILE}.html"
+      fi
+    fi
+  done
